7a7780c826f554d711d9888910bc7b0581c9576c
Brian <bellino.brian@gmail.com>
2020-01-06 20:26:10 -0500

    test

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
+  console.log('test');
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
